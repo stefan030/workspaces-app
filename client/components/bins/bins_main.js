af6552c19977d2a4c0646cba26496ebf0b0d4481
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 16:48:26 +0100

    Get the acces to the correct Bin based on the URL that user is visiting

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from './../../../imports/collections/bins';
 
 class BinsMain extends Component {
     render() {
+        console.log(this.props.bin);
         return(
             <div>BinsMain</div>
         );
     }
 }
 
-export default BinsMain;
+// Passing BinsMain's props to container wrapper
+export default createContainer((props) => {
+    const { binId } = props.match.params;
+
+    Meteor.subscribe('bins');
+
+    return { bin: Bins.findOne(binId) };
+
+}, BinsMain);
