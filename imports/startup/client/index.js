1a595288ffaecfaae303abe8a2e07569467b8a3d
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 15:14:41 +0100

    Routing with react router

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,9 +4,21 @@ import { render } from 'react-dom'
 
 import App from '../../ui/App'
 import { Bins } from './../../collections/bins';
+import { IndexRoute, browserHistory } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+import BinsMain from './../../../client/components/bins/bins_main';
+import BinsList from './../../../client/components/bins/bins_list';
 
+const routes = (
+  <BrowserRouter history={browserHistory}>
+      <App>
+          <Route path='/bins' component={BinsMain} />
+          <Route path='/bins-list' component={BinsList} />
+      </App>
+  </BrowserRouter>
+);
 
 
 Meteor.startup(() => {
-    render(<App />, document.getElementById('app'))
+    render(routes, document.getElementById('app'))
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <Header />
-                <BinsList />
+                {this.props.children}
             </div>
         )
     }
