4125a0aaa24f236aa5a50afd3523b0a3c7b3caf1
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-08 17:43:15 +0100

    Bins Share component initalization

---
+++ client/components/bins/bins_share.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class BinsShare extends Component {
+    render() {
+        return(
+            <footer className='bins-share'>
+                BinShare
+            </footer>
+        )
+    }
+}
+
+export default BinsShare;
